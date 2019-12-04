e5e675600cabef184bf3698cfbf626ebfda888b9
Leindfraust <lionheart023@yahoo.com>
2019-12-04 23:03:28 +0800

    Create admin.php

---
+++ admin.php
@@ -0,0 +1,34 @@
+<?php
+    include 'scripts/php/xmlupload.php';
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Class Card Inquiry-LCC Bais City</title>
+    <link rel="stylesheet" href="css/dependencies/bulma-0.7.1/css/bulma.min.css">
+    <link rel="stylesheet" href="css/dependencies/bulma-0.7.1/css/bulma.css.map">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <div class="hero is-fullheight is-info">
+        <div class="hero-body">
+            <div class="container">
+                
+                <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>" enctype="multipart/form-data">
+
+                    <label class="label"><p class="title">Update Database</p></label>
+                    <input type="file" name="StudentList" required><br>
+                    <br>
+                    <button class="button is-primary" value="submit" type="submit" name="submit">Submit Database</button>
+                </form>
+                
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
